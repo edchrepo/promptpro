85ae9895db1fe0e864ac4f46a192e6a48c2ec2b4
Edward Chung <edwardchung2024@gmail.com>
2023-07-17 02:11:57 -0400

    create post function added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,6 +1,55 @@
+'use client'
+
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Form from '@components/Form'
+
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  })
+
+  const createPrompt = async (e) => {
+    // prevent page reload, best prac for native react to avoid as many refreshes
+    e.preventDefault();
+    setSubmitting(true);
+    
+    try {
+      const response = await fetch('/api/prompt/new',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if(response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
   return (
-    <div>CreatePrompt</div>
+    <Form 
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
