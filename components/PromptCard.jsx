764086d5ba2d2069e834b638b975737c454b98cc
Edward Chung <edwardchung2024@gmail.com>
2023-07-26 05:25:21 -0400

    profile page and user route created

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,10 @@ import { usePathname, useRouter } from 'next/navigation';
 import postcss from 'postcss';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    const { data: session } = useSession();
+    const pathName = usePathname();
+    const router = useRouter();
+
     const [copied, setCopied] = useState("");
 
     const handleCopy = () => {
@@ -57,6 +61,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                onClick={() => handleTagClick && handleTagClick(post.tag)}>
                 {post.tag}
             </p>
+
+            {session?.user.id === post.creator._id && pathName === '/profile' && (
+                <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                    <p
+                        className="font-inter text-sm green_gradient cursor-pointer"
+                        onClick={handleEdit}
+                    >
+                        Edit
+                    </p>
+                    <p
+                        className="font-inter text-sm orange_gradient cursor-pointer"
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
